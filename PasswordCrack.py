ca02000666cb9a5a5d980daa0cc7e8864fff579b
yuer020611 <38599993+yuer020611@users.noreply.github.com>
2019-06-07 14:28:45 +0800

    Update PasswordCrack.py

--- PasswordCrack.py
+++ PasswordCrack.py
@@ -1,8 +1,6 @@
 import itertools
-#repeat是密码组合的位数，0123 ... 是密码可以组合的元素
-num = input("请输入组合位数：")
+num = input("Please enter the password length：")
 cc = int(num)
-
 passwd = ("".join(x) for x in itertools.product("0123456789QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm!@#$%^&*().", repeat=cc))
 while True:
     try:
